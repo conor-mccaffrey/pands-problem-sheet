98234852c96a620dfdb0d7cac2f7d20ab44cc07f
conor-mccaffrey <66948664+conor-mccaffrey@users.noreply.github.com>
2021-01-28 12:05:14 +0000

    BMI Calc

--- bmi.py
+++ bmi.py
@@ -6,9 +6,9 @@ print('Hello, and welcome to your personal BMI calculator.')  # message to greet
 weight = float(input('Please enter your weight in kg, in numerical format :'))   # asking for weight in kg. Converting this value to a float for calculations 
 height = float(input('Thank you, now please enter your height in cm, in numerical format :')) # asking for height in cm. Converting this value to a float for calculations 
 
-convertToMetres = height / 100 # Converting our cm value to m^2 for BMI formula
+convertToMetres = height / 100 # Converting our cm value to m^2 for BMI formula (retreived from Stack Overflow, referenced in README file)
 
 BMI = ((weight / (convertToMetres*convertToMetres)))  # now calculating the BMI based on given formula from question.
 
-print ('Your BMI is {:.2f}'.format(BMI))  # Giving the user their BMI based on input values, rouding to two decimal places as per the example
+print ('Your BMI is {:.2f}'.format(BMI))  # Giving the user their BMI based on input values, rounding to two decimal places as per the example
 print ('Thank you')   # Thanking the user for using the BMI program
