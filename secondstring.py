01b5e63ac432060f89d2cf5666f9e4859ae1d3d4
conor-mccaffrey <66948664+conor-mccaffrey@users.noreply.github.com>
2021-02-04 16:18:24 +0000

    cleaned up code

--- secondstring.py
+++ secondstring.py
@@ -4,6 +4,5 @@
 
 sentance = input('Please write a sentance') # asking user for sentance to carry out operation on
 reversed_Sentence = sentance[::-1] # reversing the input
-second_Letter = reversed_Sentence[::2] # taking every second letter from reversed sentence variable
+second_Letter = reversed_Sentence[::2] # taking every second letter from reversed sentence variable using W3 schools examples on slicing
 print(second_Letter) # printing the resulting output
-
