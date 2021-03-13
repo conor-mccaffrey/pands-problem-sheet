1189055547deaee2603fd13da5384c516f2f7263
conor-mccaffrey <66948664+conor-mccaffrey@users.noreply.github.com>
2021-03-13 15:47:06 +0000

    updaed

--- weekday.py
+++ weekday.py
@@ -4,7 +4,7 @@
 
 import datetime  #import datetime module as per W3Schools 
 
-weekDays = ['Monday','Tuesday','Wednesday','Thursday','Friday'] # List of all the weekdays
+weekDays =['Monday','Tuesday','Wednesday','Thursday','Friday'] # List of all the weekdays
 weekEnd = ['Saturday','Sunday'] # In this example, this list isn't needed but included for completeness
 
 now = datetime.date.today() # Using datetime module, this code gets us the current date
@@ -15,4 +15,4 @@ today = ('{:%A}'.format(now)) # I am only interested in the string format of the
 if today in weekDays: # checking if our current day is in the weekDays list
     print('Yes, unfortunately today is a weekday') # Print this if current day is a weekday
 elif today in weekEnd: # if current day is not a weekday, it must then be a Sat or Sun (a slight sanity check as this could easily be an 'else' clause)
-    print('It is the weekend, yay!')  # code for if day is in the weekend
+    print('It is the weekend, yay!')  # code for if day is in the weekend.
