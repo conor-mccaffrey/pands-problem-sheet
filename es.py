b328ba114b0abcb9ec268d0276e3490602c6ba45
conor-mccaffrey <66948664+conor-mccaffrey@users.noreply.github.com>
2021-03-29 18:44:52 +0100

    updated code for problem

--- es.py
+++ es.py
@@ -4,16 +4,11 @@
 
 import os   # import the os module
 
-def readLetter(file):   # define our function
-    list1 = []   # this is an empty list. We will store occurences of 'e' in here
-    if (os.path.exists(file)):  # check first that the file you are inputting exists
-            with open(file, 'rt') as f:  # open the file in read mode and assign it as 'f'
-                data = f.read() # read the contents of the file and assign to variable 'data'
-                for char in data: # for loop. For each character in our datafile perform an operation
-                    if char.lower() == 'e': # if the letter is e (I have added lower function to account for capital 'E' also)
-                        list1.append(char) # add the occurence of the letter 'e' to my list which is defined at start of the function
-                print(len(list1))    # print the length of the list therefore, the occurences of the letter 'e'
-    else: # I added this to account for errors in inputting file name
-        print('FileNotFound') 
+filetoImport = str(input("Please enter the name of the file you would like to import: "))   # the user is prompted to enter name of txt file as string
+if (os.path.exists(filetoImport)):  # check first that the file you are inputting exists
 
-readLetter('georgeOrwell.txt')
+    with open (filetoImport, 'r') as text_file: # opens text file you have entered in read mode and assigned the variable text_file
+        data = text_file.read()      # read the contents of the file and assign to variable 'data' 
+        lower_e = data.count('e') # lower_e is equal the number of occurences of lower-case 'e'
+        higher_E = data.count('E') # higher_E is equal the number of occurences of higher-case 'E'
+        print(lower_e + higher_E)   # prints the total amount of lower case 'e' and high case 'E'
